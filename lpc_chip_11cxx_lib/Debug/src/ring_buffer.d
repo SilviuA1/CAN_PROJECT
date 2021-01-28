92e04a86977b6772843b1ff35b8e96e7ce584bf3
Silviu <name_ryu@yahoo.ro>
2021-01-28 17:52:33 +0200

    Start ignoring junk

--- lpc_chip_11cxx_lib/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /home/ryu/Workspace/facultate/CAN/PROJECT/developing/lpc_chip_11cxx_lib/inc/ring_buffer.h \
- /home/ryu/Workspace/facultate/CAN/PROJECT/developing/lpc_chip_11cxx_lib/inc/lpc_types.h
-
-/home/ryu/Workspace/facultate/CAN/PROJECT/developing/lpc_chip_11cxx_lib/inc/ring_buffer.h:
-
-/home/ryu/Workspace/facultate/CAN/PROJECT/developing/lpc_chip_11cxx_lib/inc/lpc_types.h:
--- nxp_lpcxpresso_11c24_freertos_blinky/Debug/example/src/cr_startup_lpc11xx.d
+++
@@ -1,4 +0,0 @@
-example/src/cr_startup_lpc11xx.d: ../example/src/cr_startup_lpc11xx.c \
- /home/ryu/Workspace/facultate/CAN/PROJECT/developing/lpc_chip_11cxx_lib/inc/sys_config.h
-
-/home/ryu/Workspace/facultate/CAN/PROJECT/developing/lpc_chip_11cxx_lib/inc/sys_config.h:
