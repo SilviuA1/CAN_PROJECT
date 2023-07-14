8d7b78e19e302e2fad8ca2c51b0b3cdde091b3d7
SilviuA <name_ryu@yahoo.ro>
2023-07-14 17:58:39 +0300

    Initial Commit for this branch

--- nxp_lpcxpresso_11c24_freertos_blinky/example/src/freertos_blinky.c
+++ nxp_lpcxpresso_11c24_freertos_blinky/example/src/freertos_blinky.c
@@ -38,88 +38,16 @@ static void prvSetupHardware(void)
 static void blinkLed(void *pvParameters)
 {
 	bool ledState = false;
-	char my_message[30];
-
-	uint8_t what_to_send_cnt = 0u;
 	for(;;)
 	{
-//		sprintf(my_message, "Hello world\r\n");
-
-		if (what_to_send_cnt == 0u)
-		{
-			get_sensor_(DB_ID_Uart_messages, my_message);
-			Board_UARTPutSTR(my_message);
-
-		}
-		else if (what_to_send_cnt == 1u)
-		{
-			get_sensor_(DB_ID_Buttons_potentiometer, my_message);
-			Board_UARTPutSTR(my_message);
-		}
-		else if(what_to_send_cnt == 2u)
-		{
-			get_sensor_(DB_ID_Humidity_sensor, my_message);
-			Board_UARTPutSTR(my_message);
-		}
-		else if(what_to_send_cnt == 3u)
-		{
-			get_sensor_(DB_ID_Temperature, my_message);
-			Board_UARTPutSTR(my_message);
-		}
-		else
-		{
-			get_sensor_(DB_ID_Proximity_sensor, my_message);
-			Board_UARTPutSTR(my_message);
-		}
 		Board_LED_Set(0, ledState);
 		ledState = (bool)!ledState;
+		puts("blindLed task loop");
 
-		what_to_send_cnt ++;
-		what_to_send_cnt = what_to_send_cnt % 5;
-		puts("-----");
-		puts(my_message);
-		vTaskDelay(600 * 1);
-
+		vTaskDelay(configTICK_RATE_HZ * 2);
 	}
 }
 
-static void transmitPeriodic(void *pvParameters)
-{
-
-    static uint8_t test_value[8];
-    int i = 0;
-    test_value[i] = 0;
-
-	for(;;)
-	{
-		test_value[i] ++;
-		if (test_value[i] > 9u )
-		{
-			test_value[i] = 0u;
-		}
-//		LPC_CCAN_API->can_transmit(&msg_obj);
-
-//		send_TEMP_val_over_CAN(2u);
-//		vTaskDelay(configTICK_RATE_HZ);
-//
-//		send_HUMIDITY_val_over_CAN(100u);
-//		vTaskDelay(configTICK_RATE_HZ);
-//
-//		send_PROXIMITY_val_over_CAN(130u);
-//		vTaskDelay(configTICK_RATE_HZ);
-//
-//		send_UART_msg_over_CAN("heya\r\n", strlen("heya\r\n"));
-//		vTaskDelay(configTICK_RATE_HZ);
-//
-//		send_buttons_potentiometer_over_CAN(true, false, 250u);
-//		vTaskDelay(configTICK_RATE_HZ);
-//
-//
-//		update_database(CAN_ID_Temperature, test_value);
-//		update_database(CAN_ID_Uart_messages, (uint8_t*)("sadasda\n"));
-		vTaskDelay(configTICK_RATE_HZ * 3);
-	}
-}
 
 /*****************************************************************************
  * Public functions
@@ -132,10 +60,6 @@ int main(void)
 
 	xTaskCreate(blinkLed, (signed char *) "blinkLed",
 				configMINIMAL_STACK_SIZE + 80, NULL, (tskIDLE_PRIORITY + 1UL),
-				(xTaskHandle *) NULL);
-
-	xTaskCreate(transmitPeriodic, (signed char *) "periodicCanTransmit",
-				configMINIMAL_STACK_SIZE + 30, NULL, (tskIDLE_PRIORITY + 1UL),
 				(xTaskHandle *) NULL);
 
 	/* Send initial messages */
